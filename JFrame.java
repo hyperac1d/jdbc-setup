cc2e90de6b146e0795279c32ec5b37e0676732ca
Marco Mat <dlastgamer18@gmail.com>
2017-09-01 00:14:55 +0800

    java helper

--- JFrame.java
+++ JFrame.java
@@ -34,57 +34,76 @@ public Image stretchImage(String path,JLabel label)
 
 
 public void refreshTable(){
-        //Load data
-        if(SQLite.openDB()){
-            String[] columns = {"ID", "NAME", "BIRTHDAY", "IMAGE"};
-            String[][] records = SQLite.read("profile",columns);
-            javax.swing.table.DefaultTableModel model = new javax.swing.table.DefaultTableModel(records, columns);
-            this.tblProfile.setModel(model);
-            
-            //Additional COde Snippets for JTABLE behavior
-            ListSelectionListener lsl = new ListSelectionListener(){
-                public void valueChanged(javax.swing.event.ListSelectionEvent event) {
-                    if (tblProfile.getSelectedRow() > -1) {
-                        int row = tblProfile.getSelectedRow();
-                        //JOptionPane.showMessageDialog(null, "Row is " + row);
-                        if(SQLite.openDB()){
-                            String sr_id = tblProfile.getValueAt(tblProfile.getSelectedRow(), 0).toString();
-                            int id = Integer.parseInt(sr_id);
-                            String[][] result = SQLite.read("profile", "ID=" + id);
-                            
-                            txtId.setText(result[0][0]);
-                            txtName.setText(result[0][1]);
-                            String date = result[0][2];
-                            try {
-                                java.util.Date date2 = new SimpleDateFormat("MMM dd, yyy").parse(date);
-                                dcBirthdayChooser.setDate(date2);
-                            } catch (ParseException ex) {
-                                Logger.getLogger(FrmMain.class.getName()).log(Level.SEVERE, null, ex);
-                            }
-                            
-                            
-                            
-                            try{
-                                byte[] in = SQLite.read("profile", "image", id);
-                                ImageIcon icon =new ImageIcon(in);
-
-                                //stretching image
-                                Toolkit toolkit = Toolkit.getDefaultToolkit();
-                                Image image = icon.getImage();
-                                Image scaledImage = image.getScaledInstance(lblIMAGE.getWidth(),lblIMAGE.getHeight(), Image.SCALE_DEFAULT);                                                                
-                                lblIMAGE.setIcon(new javax.swing.ImageIcon(scaledImage));  
-                            }
-                            catch(Exception e){
-                                System.out.println("Error: " + e.getMessage());
-                            }
-                            
-                            SQLite.closeDB();
+    //Load data
+    if(SQLite.openDB()){
+        String[] columns = {"ID", "NAME", "BIRTHDAY", "IMAGE"};
+        String[][] records = SQLite.read("profile",columns);
+        javax.swing.table.DefaultTableModel model = new javax.swing.table.DefaultTableModel(records, columns);
+        this.tblProfile.setModel(model);
+        
+        //Additional COde Snippets for JTABLE behavior
+        ListSelectionListener lsl = new ListSelectionListener(){
+            public void valueChanged(javax.swing.event.ListSelectionEvent event) {
+                if (tblProfile.getSelectedRow() > -1) {
+                    int row = tblProfile.getSelectedRow();
+                    //JOptionPane.showMessageDialog(null, "Row is " + row);
+                    if(SQLite.openDB()){
+                        String sr_id = tblProfile.getValueAt(tblProfile.getSelectedRow(), 0).toString();
+                        int id = Integer.parseInt(sr_id);
+                        String[][] result = SQLite.read("profile", "ID=" + id);
+                        
+                        txtId.setText(result[0][0]);
+                        txtName.setText(result[0][1]);
+                        String date = result[0][2];
+                        try {
+                            java.util.Date date2 = new SimpleDateFormat("MMM dd, yyy").parse(date);
+                            dcBirthdayChooser.setDate(date2);
+                        } catch (ParseException ex) {
+                            Logger.getLogger(FrmMain.class.getName()).log(Level.SEVERE, null, ex);
                         }
+                        
+                        
+                        
+                        try{
+                            byte[] in = SQLite.read("profile", "image", id);
+                            ImageIcon icon =new ImageIcon(in);
+
+                            //stretching image
+                            Toolkit toolkit = Toolkit.getDefaultToolkit();
+                            Image image = icon.getImage();
+                            Image scaledImage = image.getScaledInstance(lblIMAGE.getWidth(),lblIMAGE.getHeight(), Image.SCALE_DEFAULT);                                                                
+                            lblIMAGE.setIcon(new javax.swing.ImageIcon(scaledImage));  
+                        }
+                        catch(Exception e){
+                            System.out.println("Error: " + e.getMessage());
+                        }
+                        
+                        SQLite.closeDB();
                     }
                 }
-            };
-            this.tblProfile.getSelectionModel().addListSelectionListener(lsl);
-            
-            SQLite.closeDB();
-        }        
-    }
+            }
+        };
+        this.tblProfile.getSelectionModel().addListSelectionListener(lsl);
+        
+        SQLite.closeDB();
+    }        
+}
+
+
+
+//Update Table - Search Key Released
+try{
+    conn = getConnection(SQLite.url);
+    stmt = conn.createStatement();
+    String result = txtSearch.getText();
+    if(txtSearch.equals("")){
+        showTable();
+    }else{
+        String query = "SELECT * FROM Guestbook WHERE NAME LIKE'"+result+"%'"+"OR ID LIKE'"+result+"%'"+"OR CONTACTNO LIKE'"+result+"%'"+"OR EMAIL LIKE'"+result+"%'"+"OR GENDER LIKE'"+result+"%'" ;
+//                String query = "SELECT * FROM Guestbook WHERE NAME LIKE '"+result+"%'";
+        ResultSet rs = stmt.executeQuery(query);
+        this.tblGuestbook.setModel(DbUtils.resultSetToTableModel(rs)); 
+	}       
+}catch(Exception e){
+   System.out.println("Error: "+e);
+}
